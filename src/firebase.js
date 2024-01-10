7b94e42cf8cd5c9bc971ba89b0910afe01cd6fc7
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-01-10 20:42:01 +0900

    add something

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
 import {
   getAuth,
   updateProfile,
